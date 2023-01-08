9c99539de0457bdb48c72d898acd2d75630de621
Gerardufoin <gerardufoin2@hotmail.com>
2023-01-08 21:14:02 +0100

    Change frouf for new frouf more froufed

--- src/Sounds.ts
+++ src/Sounds.ts
@@ -2,7 +2,7 @@ const SOUNDS = new Map<string, string[]>();
 SOUNDS.set('POP', ['pop-1.ogg', 'pop-2.ogg', 'pop-3.ogg']);
 SOUNDS.set('WHISTLE', ['whistle-1.ogg']);
 SOUNDS.set('SELL', ['sell-1.ogg', 'sell-2.ogg', 'sell-3.ogg']);
-SOUNDS.set('FROUF', ['frouf-1.ogg']);
+SOUNDS.set('FROUF', ['frouf-1.ogg', 'frouf-2.ogg']);
 
 export default function(name: string) {
     let sound = SOUNDS.get(name);
